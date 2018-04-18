6adf2fcc269885db15f19ad31338140e960582cc
Thérence F <therenceforot@orange.fr>
2018-04-18 21:56:00 +0200

    Update Builder kit

--- UltimateSheepWars/src/fr/asynchronous/sheepwars/core/kit/BuilderKit.java
+++ UltimateSheepWars/src/fr/asynchronous/sheepwars/core/kit/BuilderKit.java
@@ -3,3 +3,14 @@ package fr.asynchronous.sheepwars.core.kit;
 public class BuilderKit {
 
 }
+
+/*ON INTERACT !! Block block = event.getClickedBlock();
+if (event.getAction() == Action.RIGHT_CLICK_BLOCK && block.getType() == Material.ANVIL && Kit.getPlayerKit(player) == Kit.BUILDER) {
+	event.setCancelled(true);
+	if (!Utils.inventoryContains(player, Material.ANVIL)) {
+		block.setType(Material.AIR);
+		Sounds.playSound(player, null, Sounds.ITEM_PICKUP, 1f, 1f);
+		player.getInventory().addItem(new ItemStack(Material.ANVIL, 1));
+		player.updateInventory();
+	}
+} -- AJOUTER AU KIT BUILDER */ 
