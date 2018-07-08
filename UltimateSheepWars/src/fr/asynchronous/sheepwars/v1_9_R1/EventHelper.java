51d4805fec5c41ecab6433bdb3013aa052354470
Thérence F <therenceforot@orange.fr>
2018-07-08 12:12:22 +0200

    Update 1.9.R1 EventHelper

--- UltimateSheepWars/src/fr/asynchronous/sheepwars/v1_9_R1/EventHelper.java
+++ UltimateSheepWars/src/fr/asynchronous/sheepwars/v1_9_R1/EventHelper.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import fr.asynchronous.sheepwars.core.handler.PlayerData;
+import fr.asynchronous.sheepwars.core.data.PlayerData;
 import fr.asynchronous.sheepwars.core.manager.TeamManager;
 import fr.asynchronous.sheepwars.core.version.IEventHelper;
 import fr.asynchronous.sheepwars.v1_9_R1.entity.CustomSheep;
@@ -18,13 +18,12 @@ import net.minecraft.server.v1_9_R1.ChatHoverable.EnumHoverAction;
 public class EventHelper implements IEventHelper {
 
 	@Override
-	public Boolean onEntityTargerEvent(Entity entity, Entity target) {
+	public Boolean onEntityTargetEvent(Entity entity, Entity target) {
 		net.minecraft.server.v1_9_R1.Entity entityHandler = ((CraftEntity) entity).getHandle();
 		if ((entityHandler instanceof CustomSheep)) {
 			CustomSheep sheep = (CustomSheep) entityHandler;
 			TeamManager team = PlayerData.getPlayerData((OfflinePlayer) target).getTeam();
-			if (((sheep.getColor().ordinal() != DyeColor.LIME.ordinal()) || (team == TeamManager.SPEC)
-					|| (team == PlayerData.getPlayerData(sheep.getPlayer()).getTeam()))) {
+			if (((sheep.getColor().ordinal() != DyeColor.LIME.ordinal()) || (team == TeamManager.SPEC) || (team == PlayerData.getPlayerData(sheep.getPlayer()).getTeam()))) {
 				return true;
 			}
 		}
@@ -36,15 +35,14 @@ public class EventHelper implements IEventHelper {
 		SpecialMessage msg = new SpecialMessage("");
 		Player player = event.getPlayer();
 		TeamManager playerTeam = PlayerData.getPlayerData(player).getTeam();
-		if (spec)
-		{
-			msg.setHover(prefix + ChatColor.WHITE.toString()+ChatColor.BOLD+player.getName() + suffix, EnumHoverAction.SHOW_TEXT, hover);
-	    	msg.append(": " + event.getMessage());
+		if (spec) {
+			msg.setHover(prefix + ChatColor.WHITE.toString() + ChatColor.BOLD + player.getName() + suffix, EnumHoverAction.SHOW_TEXT, hover);
+			msg.append(": " + event.getMessage());
 			msg.sendToPlayer(online);
 		} else {
-    		msg.setHover(prefix + ((playerTeam != null) ? playerTeam.getColor() : ChatColor.GRAY) + "" + player.getName() + suffix, EnumHoverAction.SHOW_TEXT, hover);
-        	msg.append(": " + event.getMessage());
-    		msg.sendToPlayer(online);
+			msg.setHover(prefix + ((playerTeam != null) ? playerTeam.getColor() : ChatColor.GRAY) + "" + player.getName() + suffix, EnumHoverAction.SHOW_TEXT, hover);
+			msg.append(": " + event.getMessage());
+			msg.sendToPlayer(online);
 		}
 	}
 }
