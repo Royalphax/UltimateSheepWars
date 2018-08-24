967c46884a13ff0baf4952a2bade2d500eec8d76
Thérence F <therenceforot@orange.fr>
2018-08-24 10:28:22 +0200

    Initialized the support for 1.12

---
+++ UltimateSheepWars/src/fr/asynchronous/sheepwars/v1_12_R1/NMSUtils.java
@@ -0,0 +1,98 @@
+package fr.asynchronous.sheepwars.v1_12_R1;
+
+import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.Dye;
+import org.bukkit.material.MaterialData;
+
+import fr.asynchronous.sheepwars.core.message.Language;
+import fr.asynchronous.sheepwars.core.version.INMSUtils;
+import fr.asynchronous.sheepwars.v1_12_R1.util.SpecialMessage;
+import net.minecraft.server.v1_12_R1.ChatClickable.EnumClickAction;
+import net.minecraft.server.v1_12_R1.ChatHoverable;
+import net.minecraft.server.v1_12_R1.ChatMessage;
+import net.minecraft.server.v1_12_R1.ChatModifier;
+import net.minecraft.server.v1_12_R1.EntityHuman;
+import net.minecraft.server.v1_12_R1.EntityPlayer;
+import net.minecraft.server.v1_12_R1.PacketPlayOutWorldBorder;
+import net.minecraft.server.v1_12_R1.WorldBorder;
+
+public class NMSUtils implements INMSUtils {
+
+	@Override
+	public void setKiller(Entity entity, Entity killer) {
+		EntityPlayer entityKiller = ((CraftPlayer) killer).getHandle();
+		((CraftPlayer) entity).getHandle().killer = (EntityHuman) entityKiller;
+	}
+
+	@Override
+	public void setItemInHand(final ItemStack item, final Player player) {
+		player.getInventory().setItemInMainHand(item);
+	}
+
+	@Override
+	public ItemStack setIllegallyGlowing(ItemStack item, boolean activate) {
+		ItemStack copy = item.clone();
+		ItemMeta meta = copy.getItemMeta();
+		if (activate) {
+			meta.addEnchant(Enchantment.LURE, 1, true);
+			meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		} else {
+			meta.removeEnchant(Enchantment.LURE);
+			meta.removeItemFlags(ItemFlag.HIDE_ENCHANTS);
+		}
+        copy.setItemMeta(meta);
+        return copy;
+	}
+
+	@Override
+	public void displayAvailableLanguages(Player player) {
+		for (Language langs : Language.getLanguages()) {
+			SpecialMessage msg = new SpecialMessage(ChatColor.YELLOW + "- " + langs.getName() + " " + ChatColor.DARK_GRAY + "[");
+			msg.setClick(ChatColor.GREEN + "➔", EnumClickAction.RUN_COMMAND, "/lang " + langs.getLocale().replace(".yml", "")).setChatModifier(new ChatModifier().setChatHoverable(new ChatHoverable(ChatHoverable.EnumHoverAction.SHOW_TEXT, new ChatMessage(ChatColor.YELLOW + "Click to select", new Object[0]))));
+			msg.append(ChatColor.DARK_GRAY + "]");
+			msg.sendToPlayer(player);
+		}
+	}
+
+	@Override
+	public void displayRedScreen(Player player, boolean activate) {
+		if (activate) {
+			WorldBorder w = new WorldBorder();
+			w.setSize(1.0D);
+			w.setCenter(player.getLocation().getX() + 10000.0D, player.getLocation().getZ() + 10000.0D);
+			((CraftPlayer) player).getHandle().playerConnection.sendPacket(new PacketPlayOutWorldBorder(w, PacketPlayOutWorldBorder.EnumWorldBorderAction.INITIALIZE));
+		} else {
+			WorldBorder ww = new WorldBorder();
+			ww.setSize(3.0E7D);
+			ww.setCenter(player.getLocation().getX(), player.getLocation().getZ());
+			((CraftPlayer) player).getHandle().playerConnection.sendPacket(new PacketPlayOutWorldBorder(ww, PacketPlayOutWorldBorder.EnumWorldBorderAction.INITIALIZE));
+		}
+	}
+
+	@Override
+	public void setUnbreakable(final ItemMeta meta, final boolean bool) {
+		meta.spigot().setUnbreakable(bool);
+	}
+
+	@Override
+	public void setHealth(final LivingEntity ent, final Double maxHealth) {
+		ent.setMaxHealth(maxHealth);
+		if (ent instanceof Player)
+			((Player) ent).setHealthScaled(false);
+		ent.setHealth(maxHealth);
+	}
+
+	@Override
+	public MaterialData getDye(DyeColor color) {
+		return new Dye(color);
+	}
+}
---
+++ UltimateSheepWars/src/fr/asynchronous/sheepwars/v1_12_R1/ParticleSpawner.java
@@ -0,0 +1,40 @@
+package fr.asynchronous.sheepwars.v1_12_R1;
+
+import java.util.ArrayList;
+
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import fr.asynchronous.sheepwars.core.data.PlayerData;
+import fr.asynchronous.sheepwars.core.handler.Particles;
+import fr.asynchronous.sheepwars.core.version.IParticleSpawner;
+import net.minecraft.server.v1_12_R1.EnumParticle;
+import net.minecraft.server.v1_12_R1.PacketPlayOutWorldParticles;
+
+public class ParticleSpawner implements IParticleSpawner {
+
+	@Override
+	public void playParticles(Particles particle, Location location, Float fx, Float fy, Float fz, int amount,
+			Float particleData, int... list) {
+		ArrayList<OfflinePlayer> copy = new ArrayList<>(PlayerData.getParticlePlayers());
+    	if (!copy.isEmpty())
+    	{
+			PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.a(particle.getId()), true, (float)location.getX(), (float)location.getY(), (float)location.getZ(), fx, fy, fz, particleData, amount, list);
+			for (OfflinePlayer p : copy)
+			{
+				if ((p.isOnline()) && (p != null) && (p instanceof org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer))
+				{
+					((org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer)p).getHandle().playerConnection.sendPacket(packet);
+				}
+			}
+    	}
+	}
+
+	@Override
+	public void playParticles(Player player, Particles particle, Location location, Float fx, Float fy, Float fz,
+			int amount, Float particleData, int... list) {
+		PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.a(particle.getId()), true, (float)location.getX(), (float)location.getY(), (float)location.getZ(), fx, fy, fz, particleData, amount, list);
+		((org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
+	}
+}
