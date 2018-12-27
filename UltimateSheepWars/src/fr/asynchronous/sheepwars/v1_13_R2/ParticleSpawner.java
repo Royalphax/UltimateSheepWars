26b389944b0ee14f2a5b9731f0d73bb89cd6fb1e
Thérence F <therenceforot@orange.fr>
2018-12-27 21:10:40 +0100

    Improve 1.13 support but not finished yet

---
+++ UltimateSheepWars/src/fr/asynchronous/sheepwars/v1_13_R2/ParticleSpawner.java
@@ -0,0 +1,48 @@
+package fr.asynchronous.sheepwars.v1_13_R2;
+
+import java.util.ArrayList;
+
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+
+import fr.asynchronous.sheepwars.core.data.PlayerData;
+import fr.asynchronous.sheepwars.core.handler.Particles;
+import fr.asynchronous.sheepwars.core.manager.ExceptionManager;
+import fr.asynchronous.sheepwars.core.version.IParticleSpawner;
+
+public class ParticleSpawner implements IParticleSpawner {
+
+	private static boolean reportedError = false;
+
+	@Override
+	public void playParticles(Particles particle, Location location, Float fx, Float fy, Float fz, int amount, Float particleData, int... list) {
+		ArrayList<OfflinePlayer> copy = new ArrayList<>(PlayerData.getParticlePlayers());
+		if (!copy.isEmpty())
+			try {
+				for (OfflinePlayer p : copy)
+					if ((p.isOnline()) && (p != null) && (p instanceof org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer))
+						playParticles(p.getPlayer(), particle, location, fx, fy, fz, amount, particleData, list);
+			} catch (Exception ex) {
+				if (!reportedError) {
+					reportedError = true;
+					new ExceptionManager(ex).register(true);
+				}
+				// Do nothing
+			}
+	}
+
+	@Override
+	public void playParticles(Player player, Particles particle, Location location, Float fx, Float fy, Float fz, int amount, Float particleData, int... list) {
+		try {
+			player.spawnParticle(Particle.valueOf(particle.toString()), location, amount, fx, fy, fz, particleData);
+		} catch (Exception ex) {
+			if (!reportedError) {
+				reportedError = true;
+				new ExceptionManager(ex).register(true);
+			}
+			// Do nothing
+		}
+	}
+}
---
+++ UltimateSheepWars/src/fr/asynchronous/sheepwars/v1_13_R2/entity/firework/jDataWatcherItem.java
@@ -0,0 +1,87 @@
+package fr.asynchronous.sheepwars.v1_13_R2.entity.firework;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.bukkit.inventory.ItemStack;
+
+public class jDataWatcherItem<T> implements iNmsObject {
+
+	private static Class<?> clazz;
+	private static Constructor<?> constructor;
+	private static Method method_flag_set;
+
+	static {
+		try {
+			jDataWatcherItem.clazz = ProtocolUtils.getMinecraftClass("DataWatcher$Item");
+
+			jDataWatcherItem.constructor = jDataWatcherItem.clazz.getDeclaredConstructor(new Class[]{ProtocolUtils.getMinecraftClass("DataWatcherObject"), Object.class});
+			jDataWatcherItem.constructor.setAccessible(true);
+
+			jDataWatcherItem.method_flag_set = jDataWatcherItem.clazz.getDeclaredMethod("a", new Class[]{boolean.class});
+			jDataWatcherItem.method_flag_set.setAccessible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private final jDataWatcherObject<T> watcherobject;
+	private T value;
+	private boolean flag;
+
+	public jDataWatcherItem(jDataWatcherObject<T> watcherobject, T value) {
+		this.watcherobject = watcherobject;
+		this.value = value;
+		this.flag = true;
+	}
+
+	/**
+	 * WatcherObject
+	 */
+	// Get
+	public jDataWatcherObject<T> getWatcherObject() {
+		return this.watcherobject;
+	}
+
+	/**
+	 * Value
+	 */
+	// Set
+	public void setValue(T value) {
+		this.value = value;
+	}
+	// Get
+	public T getValue() {
+		return this.value;
+	}
+
+	/**
+	 * Flag
+	 */
+	// Get
+	public boolean getFlag() {
+		return this.flag;
+	}
+	// Set
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+	/**
+	 * Build
+	 */
+	@Override
+	public Object build() throws Exception {
+		Object val = null;
+
+		if (this.value instanceof ItemStack)
+			val = /*Optional.of*/(ProtocolUtils.refl_itemAsNMSCopy((ItemStack) this.value));
+		else
+			val = this.value;
+
+		Object item = jDataWatcherItem.constructor.newInstance(this.watcherobject.build(), val);
+		jDataWatcherItem.method_flag_set.invoke(item, this.flag);
+
+		return item;
+	}
+}
